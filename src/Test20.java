b8faabf4270b863890c5333a7cffdae64dbea49e
taosha1 <838220648@qq.com>
2019-08-21 15:13:19 +0800

    code

---
+++ src/Test20.java
@@ -0,0 +1,13 @@
+import java.util.Queue;
+
+public class Test20 {
+
+    public boolean isNumeric(char[] str) {
+        if (str == null || str.length == 0)
+            return false;
+        return new String(str).matches("[+-]?\\d*(\\.\\d+)?([eE][+-]?\\d+)?");
+    }
+
+
+
+}
